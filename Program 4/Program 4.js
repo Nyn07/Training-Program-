364322f7c75438ab451190a6e955b465552ba40c
Nayan Mehta <nayanmehta0709@gmail.com>
2022-12-08 13:53:18 +0530

    Same as 1 program

---
+++ Program 4/Program 4.js
@@ -0,0 +1,17 @@
+/*Same like 1 program*/
+
+const name = 'Nayan Mehta'
+const email = 'nayanmehta0709@gmail.com'
+const age = 22
+const address = '1437, B scheme no. 71'
+const phoneNo = 9575260083
+const gender = 'Male'
+const isMarried = false
+
+console.log('Name = ', name);
+console.log("Email = ",email);
+console.assert("Age = ",age);
+console.log("Address = ",address);
+console.log("Phone No = ",phoneNo);
+console.log("Gender = ",gender);
+console.log('Are you married = ',isMarried);
