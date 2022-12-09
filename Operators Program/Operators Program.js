fcf0fedd12f90d6f2df8b4260b212dbd93c80817
Nayan Mehta <nayanmehta0709@gmail.com>
2022-12-09 11:37:39 +0530

    All Operators Program

---
+++ Operators Program/Operators Program.js
@@ -0,0 +1,15 @@
+/* Operators Program*/
+
+let a = 5;
+let b = 4; 
+
+console.log("Addition of a & b =", a+b)
+console.log("Substraction of a & b =", a-b)
+console.log("Multiplication of a & b =", a*b)
+console.log("Division of a & b =", a/b)
+console.log("Exponentiation of a & b =", a**b)
+console.log("Modulous of a & b =", a%b)
+console.log("a++ =", a++) /*first this console will print then it will increse the value of a*/
+console.log ("++a = ",++a) /*first this will increse the vaue then it will print the value*/
+console.log("b-- =", b--) /*first this console will print then it will decrese the value of a*/
+console.log ("--b = ",--b) /*first this will decrese the vaue then it will print the value*/
