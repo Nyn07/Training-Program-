f4b15adbc9b6683bf995f72dc151f50b3422ad5d
Nayan Mehta <nayanmehta0709@gmail.com>
2022-12-12 21:08:11 +0530

    added boolen program

---
+++ Operators Program/Operators Program 5.js
@@ -0,0 +1,19 @@
+/*Operator Program 
+and & or program */
+
+const a = true
+const b = false
+
+//and program
+
+console.log ('Program of false and false is ',a == false && b == true)
+console.log ('Program of true and false is ',a == true && b == true)
+console.log ('Program of false and true is ',a == false && b == false)
+console.log ('Program of true and true is ',a == true && b == false)
+
+//or program
+
+console.log ('Program of false or false is ',a == false || b == true)
+console.log ('Program of true or false is ',a == true || b == false)
+console.log ('Program of false or True is ',a == false || b == false)
+console.log ('Program of true or true is ',a == true || b == false)
