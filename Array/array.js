fb96771dd3cbd1f22145da742d405b0341d2fd90
Nayan Mehta <nayanmehta0709@gmail.com>
2023-02-03 11:59:10 +0530

    modified

--- Array/array.js
+++ Array/array.js
@@ -1,5 +1,6 @@
 /* Array program of employee Details*/
 
+<<<<<<< HEAD
 const employeeDetails = [{name : 'Nayan',
 email : 'nayan@123',
 age : 23,
@@ -51,3 +52,70 @@ gender : 'male',
 isMarried : true}]
 let age = employeeDetails.filter((a) => {return a.age<=50})
 console.log (age)
+=======
+const employeeDetails = [{
+  name: 'Nayan',
+  email: 'nayan@123',
+  age: 23,
+  address: 'Gumasta Nagar',
+  phoneNo: 9575260083,
+  gender: 'male',
+  isMarried: false
+},
+{
+  name: 'Daksh',
+  email: 'Daksh@123',
+  age: 60,
+  address: 'Gopur',
+  phoneNo: 8989280208,
+  gender: 'male',
+  isMarried: true
+},
+{
+  name: 'Ritik',
+  email: 'ritik@123',
+  age: 50,
+  address: 'Sudama Nagar',
+  phoneNo: 9174333707,
+  gender: "male",
+  isMarried: true
+},
+{
+  name: 'khushi',
+  email: 'khushi@123',
+  age: 22,
+  address: '6 Bungalow',
+  phoneNo: 9827209334,
+  gender: 'female',
+  isMarried: false
+},
+{
+  name: 'Manali',
+  email: 'manali@123',
+  age: 55,
+  address: 'Sudama Nagar B',
+  phoneNo: 9039060160,
+  gender: 'female',
+  isMarried: true
+},
+{
+  name: 'Taha',
+  email: 'taha@123',
+  age: 36,
+  address: 'Moti Tabela',
+  phoneNo: 9752844239,
+  gender: 'female',
+  isMarried: false
+},
+{
+  name: 'Abhishek',
+  email: 'abhishek@123',
+  age: 28,
+  address: 'Ratlam',
+  phoneNo: 8962146566,
+  gender: 'male',
+  isMarried: true
+}]
+const age = employeeDetails.filter((a) => a.age <= 50)
+console.log(age)
+>>>>>>> 470c279... changes that has to be committed in function
--- Function Program/sorting.js
+++ Function Program/sorting.js
@@ -1,25 +1,45 @@
 //sorting with function
-const emp = [{name : 'Nayan',
-email : 'nayan@123',
-salary : 9575},
-{name : 'Daksh',
-email : 'Daksh@123',
-salary : 8989},
-{name : 'Ritik',
-email : 'ritik@123',
-salary : 9174},
-{name : 'khushi',
-email : 'khushi@123',
-salary : 9827},
-{name : 'Manali',
-email : 'manali@123',
-salary : 9039},
-{name : 'Taha',
-email : 'taha@123' ,
-salary : 9752},
-{name : 'Abhishek',
-email : 'abhishek@123',
-salary : 8962}]
+const emp = [{
+  name: 'Nayan',
+  email: 'nayan@123',
+  salary: 9575
+},
+{
+  name: 'Daksh',
+  email: 'Daksh@123',
+  salary: 8989
+},
+{
+  name: 'Ritik',
+  email: 'ritik@123',
+  salary: 9174
+},
+{
+  name: 'khushi',
+  email: 'khushi@123',
+  salary: 9827
+},
+{
+  name: 'Manali',
+  email: 'manali@123',
+  salary: 9039
+},
+{
+  name: 'Taha',
+  email: 'taha@123',
+  salary: 9752
+},
+{
+  name: 'Abhishek',
+  email: 'abhishek@123',
+  salary: 8962
+}]
 function sortEmp() {
+<<<<<<< HEAD
 return  emp.sort ((a,b)=> a.salary > b.salary ? 1 : -1)}
 console.log (so())
+=======
+  return emp.sort((a, b) => a.salary > b.salary ? 1 : -1)
+}
+console.log(sortEmp())
+>>>>>>> 470c279... changes that has to be committed in function
